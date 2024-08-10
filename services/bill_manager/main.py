e9f012ae808fba201a2c631c40ad4c76c6873f94
Gustavo Francelino <gustavo.francelino@gmail.com>
2024-08-10 20:16:51 +0100

    serve spa

--- services/bill_manager/main.py
+++ services/bill_manager/main.py
@@ -1,13 +1,20 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
-
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
 
 
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+
+class SPAStaticFiles(StaticFiles):
+    async def get_response(self, path: str, scope):
+        response = await super().get_response(path, scope)
+        if response.status_code == 404:
+            response = await super().get_response('.', scope)
+        return response
+
+app.mount('/', SPAStaticFiles(directory='spa_dist', html=True), name='spa')
+app.mount('/assets/', SPAStaticFiles(directory='spa_dist/assets', html=True), name='spa-assets')
