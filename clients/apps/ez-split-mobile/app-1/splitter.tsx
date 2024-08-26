f65952996c934c4e24a90d103ae0d0c7e7e37fe6
Gustavo Francelino <gustavo.francelino@gmail.com>
2024-08-26 22:05:27 +0100

    go back to example app for now

---
+++ clients/apps/ez-split-mobile/app-1/splitter.tsx
@@ -0,0 +1,10 @@
+import { View, Text, TextInput } from "react-native";
+
+export default function Splitter() {
+
+    return (
+        <View>
+            <Text>trest</Text>
+        </View>
+    )
+}
