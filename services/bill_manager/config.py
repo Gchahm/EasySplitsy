656dc3f91069bd709fae407bee90b0b70b0c0281
Gustavo Francelino <gustavo.francelino@gmail.com>
2024-09-14 17:16:54 +0100

    create azure script to create structure

--- services/bill_manager/config.py
+++ services/bill_manager/config.py
@@ -2,5 +2,5 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     dev_mode: bool = False
-    github_token: str
+    github_token: str = ''
     model_config = SettingsConfigDict(env_file=".env")
