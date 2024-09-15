13fc491b85ccf89a3d914221dafa65e139be6458
Gustavo Francelino <gustavo.francelino@gmail.com>
2024-09-15 01:07:07 +0100

    fix issues with env file being added to upstream

--- services/bill_manager/app/api/v1/endpoints/health.py
+++ services/bill_manager/app/api/v1/endpoints/health.py
@@ -4,13 +4,16 @@ from app.core.config import get_secrets, get_settings
 
 router = APIRouter()
 
+import logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
 
 @router.get("/")
 def health():
+    logger.info('Health check')
     settings = get_settings()
     secret_settings = get_secrets()
     return {
             'dev_mode': settings.dev_mode,
             'app_name': secret_settings.app_name,
-            'constring': settings.applicationinsights_connection_string
             }
--- services/bill_manager/app/core/logging/__init__.py
+++ services/bill_manager/app/core/logging/__init__.py
@@ -7,9 +7,12 @@ from opentelemetry.sdk.resources import SERVICE_NAME, Resource
 from app.core.config import get_settings
 from app.core.logging.azure_exporter import get_azure_exporter 
 
-
+import logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
 
 def setup_telemetry(app): 
+    logging.info("Setting up telemetry")
     settings = get_settings() 
     # Set up OpenTelemetry tracing
     resource = Resource(attributes={
@@ -19,11 +22,14 @@ def setup_telemetry(app):
     trace_provider = TracerProvider(resource=resource)
 
     if settings.dev_mode:
+        logging.info("Running in dev mode")
         console_exporter = ConsoleSpanExporter()
         span_processor = BatchSpanProcessor(console_exporter)
         trace_provider.add_span_processor(span_processor)
     else:
+        logging.info("Running in prod mode")
         span_processor = BatchSpanProcessor(get_azure_exporter())
+        logging.info("Adding span processor")
         trace_provider.add_span_processor(span_processor)
 
     # Set the global trace provider
