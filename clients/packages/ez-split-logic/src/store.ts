a6d69bd60fd734de4c0a380fdc755ed7abaa0e12
Gustavo Francelino <gustavo.francelino@gmail.com>
2024-09-28 16:12:23 +0100

    User/gutavoch/redux persist (#9)
    * improve dev server initialization
    
    * add persist for mobile
    
    * make storage work for mobile and web
    
    * make loading optional

--- clients/packages/ez-split-logic/src/store.ts
+++ clients/packages/ez-split-logic/src/store.ts
@@ -1,18 +1,35 @@
 import { Action, ThunkAction, configureStore } from "@reduxjs/toolkit";
 import { splitSlice } from "./split/splitSlice";
+import { persistCombineReducers } from "redux-persist";
+import persistStore from "redux-persist/es/persistStore";
+import storage from "redux-persist/lib/storage";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+const persistConfig = {
+  key: "root",
+  storage: typeof window === "undefined" ? AsyncStorage : storage,
+};
+
+const reducer = persistCombineReducers(persistConfig, {
+  split: splitSlice.reducer,
+});
 
 export const store = configureStore({
-    reducer: {
-        split: splitSlice.reducer,
-    },
+  reducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 });
+
+export const persistor = persistStore(store);
 
 export type AppStore = typeof store;
 export type RootState = ReturnType<AppStore["getState"]>;
 export type AppDispatch = AppStore["dispatch"];
 export type AppThunk<ThunkReturnType = void> = ThunkAction<
-    ThunkReturnType,
-    RootState,
-    unknown,
-    Action
+  ThunkReturnType,
+  RootState,
+  unknown,
+  Action
 >;
