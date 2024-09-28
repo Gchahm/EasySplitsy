977f5b0f46bbc98b83e980003376ac19aabf6ffb
Gustavo Francelino <gustavo.francelino@gmail.com>
2024-09-28 15:42:09 +0100

    add persist for mobile

--- clients/packages/ez-split-logic/src/EzSplitLogicProvider.tsx
+++ clients/packages/ez-split-logic/src/EzSplitLogicProvider.tsx
@@ -1,10 +1,23 @@
 import * as React from "react";
 import { Provider } from "react-redux";
 
-import { store } from "./store";
+import { persistor, store } from "./store";
+import { PersistGate } from "redux-persist/integration/react";
 
-export const EzSplitLogicProvider: React.FC<{ children: React.ReactNode }> = ({
+type EzSplitLogicProviderProps = {
+  children: React.ReactNode;
+  loading: React.ReactNode;
+};
+
+export const EzSplitLogicProvider: React.FC<EzSplitLogicProviderProps> = ({
   children,
+  loading,
 }) => {
-  return <Provider store={store}>{children}</Provider>;
+  return (
+    <Provider store={store}>
+      <PersistGate loading={loading} persistor={persistor}>
+        {children}
+      </PersistGate>
+    </Provider>
+  );
 };
--- clients/packages/ez-split-logic/src/store.ts
+++ clients/packages/ez-split-logic/src/store.ts
@@ -1,18 +1,34 @@
 import { Action, ThunkAction, configureStore } from "@reduxjs/toolkit";
 import { splitSlice } from "./split/splitSlice";
+import { persistCombineReducers } from "redux-persist";
+import persistStore from "redux-persist/es/persistStore";
+import storage from "@react-native-async-storage/async-storage";
+
+const persistConfig = {
+  key: "root",
+  storage: storage,
+};
+
+const reducer = persistCombineReducers(persistConfig, {
+  split: splitSlice.reducer,
+});
 
 export const store = configureStore({
-    reducer: {
-        split: splitSlice.reducer,
-    },
+  reducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 });
+
+export const persistor = persistStore(store);
 
 export type AppStore = typeof store;
 export type RootState = ReturnType<AppStore["getState"]>;
 export type AppDispatch = AppStore["dispatch"];
 export type AppThunk<ThunkReturnType = void> = ThunkAction<
-    ThunkReturnType,
-    RootState,
-    unknown,
-    Action
+  ThunkReturnType,
+  RootState,
+  unknown,
+  Action
 >;
